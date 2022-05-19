1b225b518430a3fd78bf7380cf272c70d73b8cf3
Romashkina <da_shootka@mail.ru>
2022-05-19 12:47:57 +0200

    implement solution for 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+    const path = require('path');
+    const fs = require('fs');
+
+    const textPath = path.join(__dirname, 'text.txt');
+    const stream = fs.createReadStream(textPath, 'utf-8');
+    let data = '';
+
+    stream.on('data', chunk => data += chunk);
+    stream.on('end', () => console.log(data));
+    stream.on('error', error => console.log('Error', error.message));
