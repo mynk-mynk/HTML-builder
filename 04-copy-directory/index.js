047f775a2c5000748bb8489c5697174c49b403ad
Romashkina <da_shootka@mail.ru>
2022-05-20 14:45:41 +0200

    implement solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+
+const srcPath = path.join(__dirname, 'files');
+
+fs.mkdir('./04-copy-directory/files-copy', { recursive: true }, (err) => {
+    if (err) throw err;
+});
+
+
+fs.readdir(srcPath, { withFileTypes: true }, (err, list) => {
+    if (err) throw err;
+
+    for (let item of list) {
+        if (item.isFile()) {
+            const newPath = path.join(__dirname, 'files-copy', item.name);
+            fs.copyFile(path.join(srcPath, item.name), newPath, fs.constants.COPYFILE_EXCL, (err) => {
+                if (err) throw 'File already exists';
+            })
+        }
+    }
+});
+
 
