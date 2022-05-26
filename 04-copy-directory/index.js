483d4a43f9ea15e89289ca9c51ba47b71fbaf304
Romashkina <da_shootka@mail.ru>
2022-05-26 13:05:26 +0200

    fix: add updating folder after deleting files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,10 +2,24 @@ const path = require('path');
 const fs = require('fs');
 
 const srcPath = path.join(__dirname, 'files');
+const copyPath = path.join(__dirname, 'files-copy');
 
-fs.mkdir('./04-copy-directory/files-copy', { recursive: true }, (err) => {
+
+fs.mkdir(copyPath, { recursive: true }, (err) => {
     if (err) throw err;
 });
+
+
+
+fs.readdir(copyPath, (err, list) => {
+    if (err) throw err;
+
+    for (let item of list) {
+        fs.unlink(path.join(copyPath, item), err => {
+            if (err) throw err;
+        })
+    }
+})
 
 
 fs.readdir(srcPath, { withFileTypes: true }, (err, list) => {
@@ -13,7 +27,7 @@ fs.readdir(srcPath, { withFileTypes: true }, (err, list) => {
 
     for (let item of list) {
         if (item.isFile()) {
-            const newPath = path.join(__dirname, 'files-copy', item.name);
+            const newPath = (path.join(copyPath, item.name));
             fs.copyFile(path.join(srcPath, item.name), newPath, (err) => {
                 if (err) throw 'File already exists';
             })
